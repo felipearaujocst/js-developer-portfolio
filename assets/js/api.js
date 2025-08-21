65127ea93eab7759cbd56b302fe2f4e1d9edb6b2
felipearaujo <felipearaujocst@gmail.com>
2025-08-21 12:32:34 -0300

    email

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/felipearaujocst/js-developer-portfolio/refs/heads/main/data/profile.json';
     const fetching = await fetch(url)
     return await fetching.json()
 }
